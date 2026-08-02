b3c69768095f22e0f5334a48c5b1203c38feaddc
Jesús Oviedo Riquelme <j92riquelme@gmail.com>
2026-08-02 14:42:44 -0300

    Agregar la documentación para la generación de la imagen G a partir de las imágenes A, D y E

---
+++ punto7_imagen_g.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""Punto 7: Generación de la imagen G.
+
+Enunciado:
+    ¿Cómo, de forma automática, a partir de las imágenes A, D y E,
+    usando las operaciones lógicas (AND, OR, XOR, NAND, NOR), generar
+    la imagen G de todas las células de Tipo 2 y Tipo 3?
+"""
