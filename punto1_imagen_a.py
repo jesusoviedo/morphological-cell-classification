4b94ce11e34942919f0b502cc0a79a14e697c08f
Jesús Oviedo Riquelme <j92riquelme@gmail.com>
2026-08-02 20:50:31 -0300

    Agregar función principal para descargar y procesar imagen A utilizando operaciones morfológicas y lógicas

--- punto1_imagen_a.py
+++ punto1_imagen_a.py
@@ -7,3 +7,52 @@ Enunciado:
     la inversión de imagen, generar la imagen A sin pedazos ni células
     truncadas en los bordes de la imagen?
 """
+
+import numpy as np
+
+from util import descargar_imagen
+from util import cargar_imagen_binaria
+from util import operacion_and
+from util import operacion_xor
+from util import guardar_imagen
+from util import invertir_imagen
+from util import NOMBRE_IMAGEN
+from util import reconstruccion_morfologica
+
+def main():
+    """Función principal del programa."""
+
+    print(f"Descargando imagen {NOMBRE_IMAGEN}...")
+
+    ruta_imagen = descargar_imagen()
+    print(f"Imagen {NOMBRE_IMAGEN} descargada en: {ruta_imagen}")
+
+    imagen_binaria = cargar_imagen_binaria(ruta_imagen)
+
+    print(imagen_binaria)
+
+    imagen_marco = np.zeros_like(imagen_binaria)
+
+    imagen_marco[0, :] = 255
+    imagen_marco[-1, :] = 255
+    imagen_marco[:, 0] = 255
+    imagen_marco[:, -1] = 255
+
+    print(imagen_marco)
+
+    imagen_operacion_and = operacion_and(imagen_binaria, imagen_marco)
+
+    print(imagen_operacion_and)
+
+    imagen_con_borde = reconstruccion_morfologica(imagen_operacion_and, imagen_binaria)
+
+    imagen_a = operacion_xor(imagen_binaria, imagen_con_borde)
+
+    imagen_a = invertir_imagen(imagen_a)
+
+    guardar_imagen(imagen_a, "imagen_a.png")
+
+
+
+if __name__ == "__main__":
+    main()
