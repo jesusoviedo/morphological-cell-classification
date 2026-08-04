883abc92f10c283a295395ef2e96bd1ee2d61dbd
Jesús Oviedo Riquelme <j92riquelme@gmail.com>
2026-08-04 15:47:22 -0300

    Ajustar el formato de la función main para modificar la disposición de imágenes, cambiando a 2 filas y 4 columnas.

--- punto2_imagen_b.py
+++ punto2_imagen_b.py
@@ -149,8 +149,8 @@ def main():
         lista_imagenes,
         lista_titulos,
         prefijo=PREFIJO,
-        filas=3,
-        columnas=3,
+        filas=2,
+        columnas=4,
         titulo_general="Punto 2: Agujeros de las células con citoplasma")
 
 
